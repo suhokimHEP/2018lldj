37978225e50cc25a49c599d5086d02162c82a82b
Suho Kim <skim2@fnal.gov>
2019-09-09 16:02:12 -0500

    removing unreview C files in plotters directory

--- plotters/rmplots.sh
+++
@@ -1,6 +0,0 @@
-
-rm ../plots/DY50_ALLCALOJETS_h_NoPair*
-rm ../plots/DY50_ALLCALOJETS_h_ZH*
-rm ../plots/DY50_ALLCALOJETS_h_DY*
-rm ../plots/DY50_ALLCALOJETS_h_Sig*
-rm ../plots/DY50_ALLCALOJETS_h_OffZ*

5abdfb4cde3cb03ff978b048d6493ba614d0d23d
Cristian <cristian.morgoth@gmail.com>
2020-04-10 21:28:14 +0200

    adding proper gen level infor for leptons and z boson

--- ntuples/config/submitcrab.sh
+++ ntuples/config/submitcrab.sh
@@ -1,7 +1,7 @@
 #voms-proxy-init --voms cms --valid 100:00
 
 # do we submit or just generate submit scripts
-dosubmit=false
+dosubmit=true
 doAOD=true
 dominiAOD=false
 domakeMiniAOD=false
@@ -66,6 +66,15 @@ samples=( \
 #"TTtoLfromTbar"              \
 #"DY50_1_MG"                   \
 #"DY50_1"                      \
+"QCD_HT50to100"              \
+"QCD_HT100to200"             \
+"QCD_HT200to300"             \
+"QCD_HT300to500"             \
+"QCD_HT500to700"             \
+"QCD_HT700to1000"            \
+"QCD_HT1000to1500"           \
+"QCD_HT1500to2000"           \
+"QCD_HT2000toInf"            \
 )
 #data
 

19e3ebbd8ad640a89be94cac50e2f6efc656d43f
Daniel Diaz <ddiaz006@ucr.edu>
2020-04-20 07:50:57 -0500

    Updating scripts for signal/fix makemastelist bug

--- commontools/haddjobs.sh
+++ commontools/haddjobs.sh
@@ -7,47 +7,92 @@ mkdir -p "${plotdir}/${aversion}"
 mkdir -p "${plotdir}/${aversion}/logs"
 
 samples=( \
- "Data_DoubleMuon_A"         \
- "Data_DoubleMuon_B"         \
- "Data_DoubleMuon_C"         \
- "Data_DoubleMuon_D"         \
- "Data_EGamma_A"         \
- "Data_EGamma_B"         \
- "Data_EGamma_C"         \
- "Data_EGamma_D"         \
- "Data_MuonEG_A"         \
- "Data_MuonEG_B"         \
- "Data_MuonEG_C"         \
- "Data_MuonEG_D"         \
+# "Data_DoubleMuon_A"         \
+# "Data_DoubleMuon_B"         \
+# "Data_DoubleMuon_C"         \
+# "Data_DoubleMuon_D"         \
+# "Data_EGamma_A"         \
+# "Data_EGamma_B"         \
+# "Data_EGamma_C"         \
+# "Data_EGamma_D"         \
+# "Data_MuonEG_A"         \
+# "Data_MuonEG_B"         \
+# "Data_MuonEG_C"         \
+# "Data_MuonEG_D"         \
 # "QCD_HT100to200"        \
 # "QCD_HT200to300"        \
 # "QCD_HT300to500"        \
 # "QCD_HT500to700"        \
 # "QCD_HT700to1000"       \
- "QCD_HT1000to1500"      \
- "QCD_HT1500to2000"      \
- "QCD_HT2000toInf"       \
-# "GJets_HT-40To100"        \
-# "GJets_HT-100To200"        \
-# "GJets_HT-200To400"        \
-# "GJets_HT-400To600"        \
-# "GJets_HT-600ToInf"       \
+# "QCD_HT1000to1500"      \
+# "QCD_HT1500to2000"      \
+# "QCD_HT2000toInf"       \
+## "GJets_HT-40To100"        \
+## "GJets_HT-100To200"        \
+## "GJets_HT-200To400"        \
+## "GJets_HT-400To600"        \
+## "GJets_HT-600ToInf"       \
 # "mad_DYJetsToLL_M-50"            \
- "DYJetsToLL_M-50"            \
- "WJetsToLNu"     \
- "TTJets"          \
- "ST_s-channel_4f_leptonDecays"             \
- "ST_t-channel_antitop_4f_inclusiveDecays"  \
+# "DYJetsToLL_M-50"            \
+# "WJetsToLNu"     \
+# "TTJets"          \
+# "mad_TTJets"          \
+# "TTtoLL"          \
+# "TTtoLL_mad"          \
+# "ST_s-channel_4f_leptonDecays"             \
+# "ST_t-channel_antitop_4f_inclusiveDecays"  \
 # "ST_t-channel_top_4f_inclusiveDecays"      \
- "ST_tW_antitop_5f_NoFullyHadronicDecays"   \
- "ST_tW_top_5f_NoFullyHadronicDecays"        \
- "WW"                 \
- "WZ"                 \
- "ZZ"                 \
- "WGToLNuG"              \
- "ZGTo2LG"              \
- "ZH_HToBB_ZToLL" 		\
- "ggZH_HToBB_ZToLL"		\
+# "ST_tW_antitop_5f_NoFullyHadronicDecays"   \
+# "ST_tW_top_5f_NoFullyHadronicDecays"        \
+# "WW"                 \
+# "WZ"                 \
+# "ZZ"                 \
+# "WGToLNuG"              \
+# "ZGTo2LG"              \
+# "ZH_HToBB_ZToLL" 		\
+# "ggZH_HToBB_ZToLL"		\
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-3"      \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-30"     \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-300"    \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-3"      \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-30"     \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-300"    \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-3"      \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-30"     \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-300"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-3"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-30"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-300"  \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-3"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-30"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-300"  \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-3"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-30"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-300"  \                                             
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-1"      \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-10"     \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-100"    \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-1000"   \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-1"      \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-10"     \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-100"    \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-1000"   \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-1"      \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-10"     \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-100"    \
+ "ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-1000"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-1"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-10"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-100"  \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-1000" \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-1"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-10"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-100"  \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-1000" \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-1"    \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-10"   \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-100"  \
+ "ggZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-1000" \
 )
 
 for sample in ${samples[@]}
@@ -57,9 +102,9 @@ do
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_NM1trees.sh"
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_BkgEst.sh"
  ###chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleSig_histograms.sh"    
- ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleSig_histograms.sh"    
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleSig_histograms.sh"    
  ###chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuSig_histograms.sh"    
- ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuSig_histograms.sh"    
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuSig_histograms.sh"    
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleDY_histograms.sh"    
  chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleDY_histograms.sh"    
  #chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuDY_histograms.sh"    
@@ -69,9 +114,9 @@ do
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuZH_histograms.sh"    
  chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuZH_histograms.sh"    
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleOffZ_histograms.sh"    
- ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleOffZ_histograms.sh"    
+chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleOffZ_histograms.sh"    
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuOffZ_histograms.sh"    
- ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuOffZ_histograms.sh"    
+chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuOffZ_histograms.sh"    
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleNoPair_histograms.sh"    
  ##chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuNoPair_histograms.sh"    
  chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_EleMuOSOF_histograms.sh"    
@@ -83,9 +128,9 @@ do
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_NM1trees.sh"
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_BkgEst.sh"
  ###bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleSig_histograms.sh"    
- ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleSig_histograms.sh"    
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleSig_histograms.sh"    
  ###bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuSig_histograms.sh"    
- ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuSig_histograms.sh"    
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuSig_histograms.sh"    
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleDY_histograms.sh"    
  bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleDY_histograms.sh"    
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuDY_histograms.sh"    
@@ -95,9 +140,9 @@ do
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuZH_histograms.sh"    
  bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuZH_histograms.sh"    
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleOffZ_histograms.sh"    
- ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleOffZ_histograms.sh"    
+bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoEleOffZ_histograms.sh"    
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuOffZ_histograms.sh"    
- ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuOffZ_histograms.sh"    
+bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_TwoMuOffZ_histograms.sh"    
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneEleNoPair_histograms.sh"    
  ##bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OneMuNoPair_histograms.sh"    
  bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_EleMuOSOF_histograms.sh"    
